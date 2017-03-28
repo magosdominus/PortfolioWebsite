83c12c5b94317ef451c96e54267e975fbd70157e
magosdominus <matthewcormier1993@gmail.com>
2017-03-27 23:53:49 -0400

    Add files via upload
    Added ECI app.

---
+++ CourseCodeCICSInterface/CourseCodeCICSInterface/frmInterface.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmInterface
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.txtCourseCode = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.lblCourseCode = New System.Windows.Forms.Label()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.txtError = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtDescription
+        '
+        Me.txtDescription.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.txtDescription.Enabled = False
+        Me.txtDescription.Location = New System.Drawing.Point(12, 29)
+        Me.txtDescription.Multiline = True
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(267, 129)
+        Me.txtDescription.TabIndex = 0
+        '
+        'txtCourseCode
+        '
+        Me.txtCourseCode.Location = New System.Drawing.Point(308, 29)
+        Me.txtCourseCode.MaxLength = 8
+        Me.txtCourseCode.Name = "txtCourseCode"
+        Me.txtCourseCode.Size = New System.Drawing.Size(174, 20)
+        Me.txtCourseCode.TabIndex = 1
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(308, 315)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "&Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(407, 315)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Location = New System.Drawing.Point(12, 13)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(96, 13)
+        Me.lblDescription.TabIndex = 4
+        Me.lblDescription.Text = "Course Description"
+        '
+        'lblCourseCode
+        '
+        Me.lblCourseCode.AutoSize = True
+        Me.lblCourseCode.Location = New System.Drawing.Point(305, 13)
+        Me.lblCourseCode.Name = "lblCourseCode"
+        Me.lblCourseCode.Size = New System.Drawing.Size(129, 13)
+        Me.lblCourseCode.TabIndex = 5
+        Me.lblCourseCode.Text = "Course Code (XXXX9999)"
+        '
+        'lblError
+        '
+        Me.lblError.AutoSize = True
+        Me.lblError.Location = New System.Drawing.Point(9, 193)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(80, 13)
+        Me.lblError.TabIndex = 6
+        Me.lblError.Text = "Error Messages"
+        '
+        'txtError
+        '
+        Me.txtError.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.txtError.Location = New System.Drawing.Point(12, 209)
+        Me.txtError.Multiline = True
+        Me.txtError.Name = "txtError"
+        Me.txtError.Size = New System.Drawing.Size(267, 129)
+        Me.txtError.TabIndex = 7
+        '
+        'frmInterface
+        '
+        Me.AcceptButton = Me.btnSearch
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(509, 361)
+        Me.Controls.Add(Me.txtError)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.lblCourseCode)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtCourseCode)
+        Me.Controls.Add(Me.txtDescription)
+        Me.MaximizeBox = False
+        Me.Name = "frmInterface"
+        Me.Text = "Course Code Search - CICS Interface"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents txtCourseCode As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDescription As Label
+    Friend WithEvents lblCourseCode As Label
+    Friend WithEvents lblError As Label
+    Friend WithEvents txtError As TextBox
+End Class
---
+++ CourseCodeCICSInterface/CourseCodeCICSInterface/frmInterface.vb
@@ -0,0 +1,164 @@
+﻿Imports CclECILib
+
+Public Class frmInterface
+
+    ' Declarations
+    Dim ECI As CclOECI = New CclOECI
+    Dim Connect As CclOConn = New CclOConn
+    Dim Flow As CclOFlow = New CclOFlow
+    Dim Buffer As CclOBuf = New CclOBuf
+    Dim UOW As CclOUOW = New CclOUOW
+
+    Dim DFHCOMMAREA As String   ' Output from buffer
+    Dim LK_COURSE As String     ' Course code from buffer
+    Dim LK_DESC As String       ' Description from buffer
+
+    Const TARGET_LENGTH As Integer = 8 ' Length need for valid course code
+    Const COMMAREA_LENGTH As Integer = 30
+
+    Dim errorMessage As String 'error message that is set by validation functions.
+
+    ' On load, set connection
+    Private Sub frmInterface_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Try
+
+            ' Configure connection to infinity CICS server
+            Connect.Details("Infinity", "STUDENT", "STUDENT")
+
+        Catch ex As Exception
+
+            Debug.Write("Connection Error: " + ex.Message)
+
+        End Try
+
+    End Sub
+
+    ' Validate input and submit to CICS program
+    Private Sub btnSearch_Clicked(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+
+        Try
+
+            errorMessage = ""
+
+            ' TODO: validate input
+            If (validateCourseCode(Me.txtCourseCode.Text) = True) Then
+
+                Me.txtError.Text = errorMessage
+
+                ' Fill buffer
+                Buffer.SetString(Me.txtCourseCode.Text.ToString)
+                Buffer.SetLength(COMMAREA_LENGTH)
+
+                ' Attempt to execute CICS 
+                Connect.Link(Flow, "ZZPRGCC", Buffer, UOW)
+
+                ' Recieve and parse output                
+                ' Display error message or result
+                If (String.Compare(Buffer.ExtractString(8, 16), "COURSE NOT FOUND") = 0) Then
+
+                    errorMessage = "Course '" + Buffer.ExtractString(0, 8) + "' not found in database."
+                    txtDescription.Text = ""
+                    txtError.Text = errorMessage
+
+                ElseIf (Buffer.ExtractString(0, 7) = "SQLERR:") Then
+
+                    errorMessage = "SQL error: " + Buffer.ToString
+                    txtDescription.Text = ""
+                    txtError.Text = errorMessage
+
+                Else
+
+                    LK_COURSE = Buffer.ExtractString(0, 7)
+
+                    LK_DESC = Buffer.ExtractString(8)
+
+                    txtDescription.Text = "Course description found for " + LK_COURSE + ": " + LK_DESC
+
+                End If
+
+                ' UOW.commit to end the transaction 
+                UOW.Commit(Flow)
+
+            Else
+
+                Me.txtError.Text = errorMessage
+
+            End If
+
+        Catch ex As Exception
+
+            errorMessage = "Unknown Error: " + ex.Message
+            Me.txtError.Text = errorMessage
+
+        End Try
+
+    End Sub
+
+    ' Close form
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+
+    ' Validates passed course codes.
+    '
+    ' @param string, the coursecode
+    ' @return boolean, true is valid
+    Private Function validateCourseCode(courseCode As String) As Boolean
+
+        Dim isValid As Boolean = False
+
+        If (String.IsNullOrEmpty(courseCode) = False) Then
+
+            ' Check if the course code is the correctr length.
+            If (courseCode.Length = TARGET_LENGTH) Then
+
+                ' Validate formatting, XXXX9999
+                If (Char.IsLetter(courseCode.Chars(0)) And Char.IsLetter(courseCode.Chars(1)) And Char.IsLetter(courseCode.Chars(2)) And Char.IsLetter(courseCode.Chars(3))) Then
+
+                    If (IsNumeric(courseCode.Chars(4))) And IsNumeric(courseCode.Chars(5)) And IsNumeric(courseCode.Chars(6)) And IsNumeric(courseCode.Chars(7)) Then
+
+                        isValid = True
+
+                        errorMessage = ""
+
+                        Return isValid
+
+                    Else
+
+                        errorMessage = "Input Error: The last four course code characters must be numeric. You entered '" + courseCode + "'. Correct formatting would look like 'XXXX9999'."
+                        isValid = False
+
+                        Return isValid
+
+                    End If
+
+                Else
+
+                        errorMessage = "Input Error: The first four course code characters must be alphabetic. You entered '" + courseCode + "'. Correct formatting would look like 'XXXX9999'."
+                    isValid = False
+
+                    Return isValid
+                End If
+
+            Else
+
+                errorMessage = "Input Error: Course code must be exactly 8 characters long. You entered an input of " & Len(courseCode).ToString & " characters."
+                isValid = False
+
+                Return isValid
+            End If
+
+        Else
+
+            errorMessage = "Input Error: Course code cannot be null."
+            Return isValid
+
+        End If
+
+        Return isValid
+
+    End Function
+End Class
